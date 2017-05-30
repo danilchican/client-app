626841bad8f58eb1f96217cba3c9c9c7bc54aa56
danilchican <danilchican@mail.ru>
2017-05-30 23:45:20 +0300

    Created payments migration and model

---
+++ app/Payment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'owner_id',
+        'name',
+        'number',
+        'currency',
+        'amount'
+    ];
+
+    /**
+     * An payment is owned by a user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Getting payments for a user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function payments()
+    {
+        return $this->hasMany(Payment::class, 'owner_id');
+    }
 }
